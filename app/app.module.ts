5281e5d3670674135fe170d9fb97f61a6759fa96
abelmeos <amerchan@sura.com.co>
2017-08-10 16:16:17 -0500

    first commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import ApplicationComponent from './components/application/application';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [ApplicationComponent],
-    bootstrap: [ApplicationComponent]
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app/components/application/application.ts
+++ app/components/application/application.ts
@@ -2,15 +2,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'auction-application',
-    templateUrl: 'app/components/application/application.html',
-    styleUrls: ['app/components/application/application.css'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['app/components/application/application.css']
 })
 
 export default class ApplicationComponent {
-    products: Array<Product> = [];
 
-    constructor(private productService: ProductService) {
-        this.products = this.productService.getProducts();
+    constructor() {
+
     }
 }
